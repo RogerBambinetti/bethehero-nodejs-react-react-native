b0ca2f94226ea32f303ca2208ca2fc9c06779859
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-09-28 17:43:40 -0300

    update new incident page

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -7,7 +9,24 @@ import Logo from '../../assets/logo.svg'
 
 export default function NewIncident() {
     return (
-        <div className="newIncident-container">
+        <div className="new-incident-container">
+            <div className="content">
+                <section className="form">
+                    <img src={Logo} alt="Logo" />
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+                    <Link to="/">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para Home
+                    </Link>
+                </section>
+                <form>
+                    <input type="text" placeholder="Título do caso" />
+                    <textarea placeholder="Descrição" />
+                    <input type="textArea" placeholder="Valor em reais" />
+                    <button type="submit" className="button">Cadastrar</button>
+                </form>
+            </div>
         </div>
     )
 }
