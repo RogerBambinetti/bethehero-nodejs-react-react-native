9df1fd2c711bbac9d55fb744b48ae994b28f0cc8
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-10-18 22:12:25 -0300

    update profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
@@ -6,9 +6,21 @@ import './styles.css';
 
 import Heroes from '../../assets/heroes.png'
 import Logo from '../../assets/logo.svg'
+import { useState } from 'react';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
     const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+
+    useEffect(() => {
+        api.get('profile', { headers: { Authorization: ongId } })
+            .then(response => {
+                setIncidents(response.data);
+            });
+    }, [ongId]);
+
     return (
         <div className="profile-container">
             <header>
@@ -21,20 +33,22 @@ export default function Profile() {
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                {incidents.map(i => (
+                    <li key={i.id}>
+                        <strong>CASO:</strong>
+                        <p>{i.title}</p>
 
-                    <strong>DESCRIÇÃO</strong>
-                    <p>teste teste teste</p>
+                        <strong>DESCRIÇÃO</strong>
+                        <p>{i.description}</p>
 
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
+                        <strong>VALOR:</strong>
+                        <p>{i.value}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     )
