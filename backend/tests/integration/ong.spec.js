fbcafe7c1ca7d39d878b5892564c21d02e7df3ee
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-12-17 21:45:00 -0300

    backend add rollback ong integration test

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -4,7 +4,12 @@ const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
     beforeEach(async () => {
+        await connection.migrate.rollback();
         await connection.migrate.latest();
+    });
+
+    afterAll(() => {
+        connection.destroy();
     });
 
     it('should be able to create a new ONG', async () => {
@@ -17,7 +22,8 @@ describe('ONG', () => {
                 city: 'ibirama',
                 uf: 'sc'
             });
-        console.log(response.data);
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
     });
 });
 
